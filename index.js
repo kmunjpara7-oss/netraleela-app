964fc4bd2a8f580798cbe6e40b2a1778b09ee81f
kmunjpara7-oss <kmunjpara7@gmail.com>
2026-05-26 20:14:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(cors());
 
 // Read HTML file
